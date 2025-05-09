aa21cd25603ffd183afceaff1784afe8431d660a
Edward PRITCHARD <epritchard@atreal.fr>
2025-05-09 22:39:16 +0200

    sql: clean japan jobs

--- var/setupJapon1555_jobs.sql
+++ var/setupJapon1555_jobs.sql
@@ -11,24 +11,17 @@ INSERT INTO powers ( name, enquete, attack, defence) VALUES
     -- ('', -1,0,2) ('', -1,2,1), ('', 2,-1,0),('', 2,0,-1), ('', 0,2,-1),
 
     -- ('', 1,0,0), => Enqueteurs
-    ('Plombier', 1,0,0)
-    ,('Electricien', 1,0,0)
-    -- ('', 0,1,1), => Combattants
-    ,('Agent de sécurité', 0,1,1)
-    ,('Pompier', 0,1,1)
-    -- ('', 1,1,-1), => Glass cannons
-    ,('Policier', 1,1,-1)
+    ('Ninja', 1,0,0)
+    ,('Geisha', 1,0,0)
+    ,('Acteur Nô', 1,0,0)
+    ,('Moine', 1,1,-1)
     ,('Voleur', 1,1,-1)
+    -- ('', 0,1,1), => Combattants
+    ,('Ishin shishi', 0,1,1)
+    ,('Hitokiri', 0,1,1)
+    -- ('', 1,1,-1), => Glass cannons
     -- ('', 2,0,-1), => Maitres Enqueteurs
-    ,('Conducteur de taxi', 2,0,-1)
-    ,('SDF', 2,0,-1)
     -- ('', -1,2,1), => Maitres Combattants
-    ,('Militaire', -1,2,1)
-    ,('CRS', -1,2,1)
-    --Violent 
-    -- Garde du Corps, Garde 
-    -- Enqueteurs
-    -- Professeur (Physique), Directeur, Gardien, Agent de tourisme, Trader, Voiturier, 
 ;
 
 /*
