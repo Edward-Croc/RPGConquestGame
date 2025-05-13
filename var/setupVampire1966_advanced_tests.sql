661f1cb51b7211c2b0fca26fad1870eef92e7045
Edward PRITCHARD <epritchard@atreal.fr>
2025-05-13 22:19:12 +0200

    tests : add test workers

--- var/setupVampire1966_advanced_tests.sql
+++ var/setupVampire1966_advanced_tests.sql
@@ -2,136 +2,156 @@
 -- UPDATE config SET value = 0 WHERE name in ( 'REPORTDIFF0', 'REPORTDIFF1', 'REPORTDIFF2', 'REPORTDIFF3');
 
 INSERT INTO workers (firstname, lastname, origin_id, zone_id) VALUES
+    -- Base test claim, passive, investigate
     ('Harvey', 'Matthews', (SELECT ID FROM worker_origins WHERE name = 'Angleterre'), (SELECT ID FROM zones WHERE name = 'Palazzo Pitti')),
     ('Andrei', 'Popescu', (SELECT ID FROM worker_origins WHERE name = 'Roumanie'), (SELECT ID FROM zones WHERE name = 'Palazzo Pitti')),
-    ('Indro', 'Lombardi', (SELECT ID FROM worker_origins WHERE name = 'Firenze'), (SELECT ID FROM zones WHERE name = 'Palazzo Pitti'));
+    ('Indro', 'Lombardi', (SELECT ID FROM worker_origins WHERE name = 'Firenze'), (SELECT ID FROM zones WHERE name = 'Palazzo Pitti')),
+    -- Multi claim, should be violent
+    ('Amerigo', 'Martino', (SELECT ID FROM worker_origins WHERE name = 'Firenze'), (SELECT ID FROM zones WHERE name = 'Railway Station')),
+    ('Roman', 'Aliev', (SELECT ID FROM worker_origins WHERE name = 'Roumanie'), (SELECT ID FROM zones WHERE name = 'Railway Station')),
+    ('Hortensio', 'Honorius', (SELECT ID FROM worker_origins WHERE name = 'Venezia'), (SELECT ID FROM zones WHERE name = 'Railway Station')),
+    -- Attacking ?
+    ('Maria', 'Ionescu', (SELECT ID FROM worker_origins WHERE name = 'Roumanie'), (SELECT ID FROM zones WHERE name = 'Fortezza Basso')),
+    ('Mercury', 'Messala', (SELECT ID FROM worker_origins WHERE name = 'Firenze'), (SELECT ID FROM zones WHERE name = 'Fortezza Basso')),
+    ('Maria', 'Marotta', (SELECT ID FROM worker_origins WHERE name = 'Venezia'), (SELECT ID FROM zones WHERE name = 'Fortezza Basso'));
 
 INSERT INTO controler_worker (controler_id, worker_id) VALUES
+    -- Base test claim, passive, investigate
     (
         (SELECT ID FROM controlers WHERE lastname in ('Mazzino', 'Ricciotti')),
         (SELECT ID FROM workers WHERE lastname in ('Matthews'))
     ), (
-        (SELECT ID FROM controlers WHERE lastname in ('Walkil', 'Vizirof')),
-        (SELECT ID FROM workers WHERE lastname in ('Popescu'))
-    ), (
         (SELECT ID FROM controlers WHERE lastname in ('Calabreze')),
         (SELECT ID FROM workers WHERE lastname in ('Lombardi'))
-    );
-
-INSERT INTO worker_actions (worker_id, controler_id, turn_number, zone_id, action_choice) VALUES 
-    (
-        (SELECT ID FROM workers WHERE lastname = 'Matthews'),
-        (SELECT controler_id FROM controler_worker JOIN workers ON workers.ID = controler_worker.worker_id WHERE workers.lastname = 'Matthews'),
-        0, (SELECT ID FROM zones WHERE name = 'Palazzo Pitti'),
-        'claim'
-    ),
-    (
-        (SELECT ID FROM workers WHERE lastname = 'Popescu'),
-        (SELECT controler_id FROM controler_worker JOIN workers ON workers.ID = controler_worker.worker_id WHERE workers.lastname = 'Popescu'),
-        0, (SELECT ID FROM zones WHERE name = 'Palazzo Pitti'),
-        'passive'
-    ),
-    (
-        (SELECT ID FROM workers WHERE lastname = 'Lombardi'),
-        (SELECT controler_id FROM controler_worker JOIN workers ON workers.ID = controler_worker.worker_id WHERE workers.lastname = 'Lombardi'),
-         0, (SELECT ID FROM zones WHERE name = 'Palazzo Pitti'),
-        'investigate'
+    ), (
+        (SELECT ID FROM controlers WHERE lastname in ('Walkil', 'Vizirof')),
+        (SELECT ID FROM workers WHERE lastname in ('Popescu'))
+    )
+    -- Multi claim, should be violent
+    ,(
+        (SELECT ID FROM controlers WHERE lastname in ('da Firenze')),
+        (SELECT ID FROM workers WHERE lastname in ('Honorius'))
+    ), (
+        (SELECT ID FROM controlers WHERE lastname in ('Walkil', 'Vizirof')),
+        (SELECT ID FROM workers WHERE lastname in ('Aliev'))
+    ), (
+        (SELECT ID FROM controlers WHERE lastname in ('Mazzino', 'Ricciotti')),
+        (SELECT ID FROM workers WHERE lastname in ('Martino'))
+    )
+    -- Attacking ?
+    ,(
+        (SELECT ID FROM controlers WHERE lastname in ('da Firenze')),
+        (SELECT ID FROM workers WHERE lastname in ('Messala'))
+    ), (
+        (SELECT ID FROM controlers WHERE lastname in ('Walkil', 'Vizirof')),
+        (SELECT ID FROM workers WHERE lastname in ('Ionescu'))
+    ), (
+        (SELECT ID FROM controlers WHERE lastname in ('Mazzino', 'Ricciotti')),
+        (SELECT ID FROM workers WHERE lastname in ('Marotta'))
     )
 ;
 
--- Add base powers to the workers :
-INSERT INTO worker_powers (worker_id, link_power_type_id) VALUES
--- Matthews
-    ((SELECT ID FROM workers WHERE lastname = 'Matthews'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Alcoolique'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Matthews'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Policier'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Matthews'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Célérité'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Matthews'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Puissance'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Matthews'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Vampire nouveau né'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Matthews'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Goule'
-    )),
--- Popescu
-    ((SELECT ID FROM workers WHERE lastname = 'Popescu'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Punk à chien'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Popescu'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Militaire'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Popescu'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Quiétus'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Popescu'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Puissance'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Popescu'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Vampire nouveau né'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Popescu'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Goule'
-    )),
--- Lombardi
-    ((SELECT ID FROM workers WHERE lastname = 'Lombardi'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Astrologue Amateur'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Lombardi'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Étudiant (Arts, Lettres)'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Lombardi'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Aliénation'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Lombardi'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Augure'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Lombardi'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Goule'
-    )),
-    ((SELECT ID FROM workers WHERE lastname = 'Lombardi'), (
-        SELECT link_power_type.ID FROM link_power_type
-        JOIN powers on  powers.ID = link_power_type.power_id
-        WHERE powers.name = 'Vampire nouveau né'
-    ))
-;
+-- Add actions to the workers :
+INSERT INTO worker_actions (
+    worker_id, controler_id, turn_number, zone_id, action_choice, action_params
+)
+SELECT
+    w.id,
+    cw.controler_id,
+    0,
+    w.zone_id,
+    entry.action_choice,
+    entry.action_params::json
+FROM (
+    -- Base test claim, passive, investigate
+    SELECT 'Matthews' AS lastname, 'claim' AS action_choice, '{"claim_controler_id":"2"}' AS action_params
+    UNION ALL
+    SELECT 'Popescu', 'passive', '{}'  -- empty JSON object
+    UNION ALL
+    SELECT 'Lombardi', 'investigate', '{}'
+    -- Multi claim, should be violent
+    UNION ALL
+    SELECT 'Honorius', 'claim', '{"claim_controler_id":"2"}'
+    UNION ALL
+    SELECT 'Aliev', 'claim', '{"claim_controler_id":"2"}'
+    UNION ALL
+    SELECT 'Martino', 'claim', '{"claim_controler_id":"2"}'
+    -- Attacking ?
+    UNION ALL
+    SELECT 'Messala', 'investigate', '{}'
+    UNION ALL
+    SELECT 'Ionescu', 'investigate', '{}'
+    UNION ALL
+    SELECT 'Marotta', 'investigate', '{}'
+) AS entry
+JOIN workers w ON w.lastname = entry.lastname
+JOIN controler_worker cw ON cw.worker_id = w.id;
+
+-- Add powers to the workers :
+INSERT INTO worker_powers (worker_id, link_power_type_id)
+SELECT 
+    w.id AS worker_id,
+    lpt.id AS link_power_type_id
+FROM 
+    workers w
+JOIN (
+    SELECT 'Matthews' AS lastname, 'Alcoolique' AS power_name
+    UNION ALL SELECT 'Matthews', 'Policier'
+    UNION ALL SELECT 'Matthews', 'Célérité'
+    UNION ALL SELECT 'Matthews', 'Puissance'
+    UNION ALL SELECT 'Matthews', 'Vampire nouveau né'
+    UNION ALL SELECT 'Matthews', 'Goule'
+    
+    UNION ALL SELECT 'Popescu', 'Punk à chien'
+    UNION ALL SELECT 'Popescu', 'Militaire'
+    UNION ALL SELECT 'Popescu', 'Quiétus'
+    UNION ALL SELECT 'Popescu', 'Puissance'
+    UNION ALL SELECT 'Popescu', 'Vampire nouveau né'
+    UNION ALL SELECT 'Popescu', 'Goule'
+
+    UNION ALL SELECT 'Lombardi', 'Astrologue Amateur'
+    UNION ALL SELECT 'Lombardi', 'Étudiant (Arts, Lettres)'
+    UNION ALL SELECT 'Lombardi', 'Aliénation'
+    UNION ALL SELECT 'Lombardi', 'Augure'
+    UNION ALL SELECT 'Lombardi', 'Goule'
+    
+    UNION ALL SELECT 'Aliev', 'Détective privé'
+    UNION ALL SELECT 'Aliev', 'Militaire réserviste'
+    UNION ALL SELECT 'Aliev', 'Aliénation'
+    UNION ALL SELECT 'Aliev', 'Célérité'
+    UNION ALL SELECT 'Aliev', 'Goule'
+    UNION ALL SELECT 'Aliev', 'Vampire nouveau né'
+    
+    UNION ALL SELECT 'Honorius', 'SDF'
+    UNION ALL SELECT 'Honorius', 'Hooligan'
+    UNION ALL SELECT 'Honorius', 'Endurance'
+    UNION ALL SELECT 'Honorius', 'Puissance'
+    UNION ALL SELECT 'Honorius', 'Vampire nouveau né'
+    
+    UNION ALL SELECT 'Martino', 'Ouvrier du bâtiment'
+    UNION ALL SELECT 'Martino', 'Alcoolique'
+    UNION ALL SELECT 'Martino', 'Nécromancie'
+    UNION ALL SELECT 'Martino', 'Goule'
+    UNION ALL SELECT 'Martino', 'Vampire nouveau né'
+
+    UNION ALL SELECT 'Messala', 'Militaire réserviste'
+    UNION ALL SELECT 'Messala', 'Pompier'
+    UNION ALL SELECT 'Messala', 'Célérité'
+    UNION ALL SELECT 'Messala', 'Augure'
+    UNION ALL SELECT 'Messala', 'Goule'
+    
+    UNION ALL SELECT 'Ionescu', 'Ouvrier du bâtiment'
+    UNION ALL SELECT 'Ionescu', 'Alcoolique'
+    UNION ALL SELECT 'Ionescu', 'Nécromancie'
+    UNION ALL SELECT 'Ionescu', 'Goule'
+    UNION ALL SELECT 'Ionescu', 'Vampire nouveau né'
+
+    UNION ALL SELECT 'Marotta', 'Militaire réserviste'
+    UNION ALL SELECT 'Marotta', 'Pompier'
+    UNION ALL SELECT 'Marotta', 'Célérité'
+    UNION ALL SELECT 'Marotta', 'Augure'
+    UNION ALL SELECT 'Marotta', 'Goule'
+) AS wp ON wp.lastname = w.lastname
+JOIN powers p ON p.name = wp.power_name
+JOIN link_power_type lpt ON lpt.power_id = p.id;
+
