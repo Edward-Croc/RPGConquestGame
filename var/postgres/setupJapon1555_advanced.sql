f84b7ce5263b8f482b1c73d36ed69d2434f8b0ff
Edward PRITCHARD <epritchard@atreal.fr>
2025-06-23 19:22:55 +0200

    config(japan) :  add the servants for the Shogun used during the 1st game

--- var/postgres/setupJapon1555_advanced.sql
+++ var/postgres/setupJapon1555_advanced.sql
@@ -165,3 +165,111 @@ JOIN (
 ) AS wp ON wp.lastname = w.lastname
 JOIN powers p ON p.name = wp.power_name
 JOIN link_power_type lpt ON lpt.power_id = p.id;
+
+
+-- Create the Shougunat start workers
+/*
+Asakura Mitsunao-dono (朝倉 光直)
+Tsūshi (通使) – Diplomate ou émissaire
+Go-ban (碁盤) – Plateau de Go
+
+Ibara-dono  (茨の紅)
+Ninja-kahō (忍者家法) – Membre d’une lignée de ninjas
+Shamisen (三味線) – Instrument à cordes
+
+Takeda Renryū-dono  (武田 蓮竜)
+Kuro-hatamoto (黒旗本) – Garde d’élite
+Katana (刀) – L’arme emblématique du samouraï
+
+Sōen-dono  (僧円)
+Biwa Hōshi (琵琶法師) – Conteur aveugle itinérant
+Chadōgu (茶道具) – Ustensiles du thé
+
+*/
+WITH names_data(firstname, lastname, origin_name, zone_name) AS (
+    VALUES
+        ('Asakura(朝倉)', 'Mitsunao-dono(光直-殿)', 'Honshu - Kyoto', 'Cité Impériale de Kyoto'),
+        ('', 'Ibara-dono(茨の紅-殿)', 'Honshu - Kyoto', 'Cité Impériale de Kyoto'),
+        ('Renryū-dono(蓮竜-殿)', 'Renryū-dono(蓮竜-殿)', 'Honshu - Kyoto', 'Cité Impériale de Kyoto'),
+        ('', 'Sōen-dono(僧円-殿)', 'Honshu - Kyoto', 'Cité Impériale de Kyoto')
+)
+INSERT INTO workers (firstname, lastname, origin_id, zone_id)
+SELECT
+    nd.firstname,
+    nd.lastname,
+    wo.id AS origin_id,
+    z.id AS zone_id
+FROM names_data nd
+JOIN worker_origins wo ON wo.name = nd.origin_name
+JOIN zones z ON z.name = nd.zone_name;
+
+
+-- Now, get controller ID once
+-- Then add the links
+WITH controller AS (
+    SELECT id AS controller_id FROM controllers WHERE lastname = 'Ashikaga (足利)'
+),
+worker_ids AS (
+    SELECT id AS worker_id FROM workers WHERE lastname IN ('Mitsunao-dono(光直-殿)', 'Ibara-dono(茨の紅-殿)', 'Renryū-dono(蓮竜-殿)', 'Sōen-dono(僧円-殿)')
+)
+INSERT INTO controller_worker (controller_id, worker_id)
+SELECT controller.controller_id, worker_ids.worker_id
+FROM controller, worker_ids;
+
+-- Add actions to the workers :
+INSERT INTO worker_actions (
+    worker_id, controller_id, turn_number, zone_id, action_choice, action_params
+)
+SELECT
+    w.id,
+    cw.controller_id,
+    0,
+    w.zone_id,
+    entry.action_choice,
+    entry.action_params::json
+FROM (
+    SELECT 'Mitsunao-dono(光直-殿)' AS lastname, 'passive' AS action_choice, '{}' AS action_params
+    UNION ALL SELECT 'Ibara-dono(茨の紅-殿)', 'passive', '{}'
+    UNION ALL SELECT  'Renryū-dono(蓮竜-殿)', 'passive', '{}'
+    UNION ALL SELECT  'Sōen-dono(僧円-殿)', 'passive', '{}'
+) AS entry
+JOIN workers w ON w.lastname = entry.lastname
+JOIN controller_worker cw ON cw.worker_id = w.id;
+;
+
+-- Add powers to the workers :
+INSERT INTO worker_powers (worker_id, link_power_type_id)
+SELECT
+    w.id AS worker_id,
+    lpt.id AS link_power_type_id
+FROM
+    workers w
+JOIN (
+    SELECT 'Sōen-dono(僧円-殿)' AS lastname, 'Biwa Hōshi (琵琶法師) – Conteur aveugle itinérant' AS power_name
+    UNION ALL SELECT 'Sōen-dono(僧円-殿)', 'Chadōgu (茶道具) – Ustensiles du thé'
+    UNION ALL SELECT 'Sōen-dono(僧円-殿)', 'Kadō / Ikebana (華道 / 生け花) – Art floral'
+    UNION ALL SELECT 'Sōen-dono(僧円-殿)', 'Chadō (茶道) – Voie du thé'
+    UNION ALL SELECT 'Sōen-dono(僧円-殿)', 'Bugaku (舞楽) – Danse de cour'
+    UNION ALL SELECT 'Sōen-dono(僧円-殿)', 'Encens Coréen'
+    UNION ALL SELECT 'Renryū-dono(蓮竜-殿)', 'Kuro-hatamoto (黒旗本) – Garde d’élite'
+    UNION ALL SELECT 'Renryū-dono(蓮竜-殿)', 'Katana (刀) – L’arme emblématique du samouraï'
+    UNION ALL SELECT 'Renryū-dono(蓮竜-殿)', 'Shodō (書道) – Calligraphie'
+    UNION ALL SELECT 'Renryū-dono(蓮竜-殿)', 'Iaijutsu (居合術) – Art de dégainer et frapper en un mouvement'
+    UNION ALL SELECT 'Renryū-dono(蓮竜-殿)', 'Bajutsu (馬術) – Art de l’équitation militaire'
+    UNION ALL SELECT 'Renryū-dono(蓮竜-殿)', 'Cheval Kagawa'
+    UNION ALL SELECT 'Ibara-dono(茨の紅-殿)', 'Ninja-kahō (忍者家法) – Membre d’une lignée de ninjas'
+    UNION ALL SELECT 'Ibara-dono(茨の紅-殿)', 'Shamisen (三味線) – Instrument à cordes'
+    UNION ALL SELECT 'Ibara-dono(茨の紅-殿)', 'Kyūjutsu (弓術) – Art du tir à l’arc'
+    UNION ALL SELECT 'Ibara-dono(茨の紅-殿)', 'Shodō (書道) – Calligraphie'
+    UNION ALL SELECT 'Ibara-dono(茨の紅-殿)', 'Bugaku (舞楽) – Danse de cour'
+    UNION ALL SELECT 'Ibara-dono(茨の紅-殿)', 'Chadō (茶道) – Voie du thé'
+    UNION ALL SELECT 'Ibara-dono(茨の紅-殿)', 'Thé d’Oboké et d’Iya'
+    UNION ALL SELECT 'Mitsunao-dono(光直-殿)', 'Tsūshi (通使) – Diplomate ou émissaire'
+    UNION ALL SELECT 'Mitsunao-dono(光直-殿)', 'Go-ban (碁盤) – Plateau de Go'
+    UNION ALL SELECT 'Mitsunao-dono(光直-殿)', 'Armure en fer de Kochi'
+    UNION ALL SELECT 'Mitsunao-dono(光直-殿)', 'Heihō (兵法) – Stratégie militaire'
+    UNION ALL SELECT 'Mitsunao-dono(光直-殿)', 'Bugaku (舞楽) – Danse de cour'
+    UNION ALL SELECT 'Mitsunao-dono(光直-殿)', 'Shodō (書道) – Calligraphie'
+) AS wp ON wp.lastname = w.lastname
+JOIN powers p ON p.name = wp.power_name
+JOIN link_power_type lpt ON lpt.power_id = p.id;
