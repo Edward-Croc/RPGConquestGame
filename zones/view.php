2989e3fc58d4a6c1962d31a7a78a575956137246
Edward 'Croc' P <croc.edward@gmail.com>
2025-07-04 23:12:52 +0200

    feat : upgrade zone view to bulma

--- zones/view.php
+++ zones/view.php
@@ -1,32 +1,61 @@
 <?php
     $zones = getZonesArray($gameReady);
-    $imgString = sprintf('<img src="/%s/img/%s" alt="%s" style="max-width:%s; height:auto;">',  $_SESSION['FOLDER'], getConfig($gameReady, 'map_file'), getConfig($gameReady, 'map_alt'), '100%');
+    $imgString = sprintf(
+        '<img src="/%s/img/%s" alt="%s" style="max-width:100%%; height:auto;">',
+        $_SESSION['FOLDER'],
+        htmlspecialchars(getConfig($gameReady, 'map_file')),
+        htmlspecialchars(getConfig($gameReady, 'map_alt'))
+    );
 ?>
 
-<div class="zones">
-    <h2>Zones</h2>
-    <h4 onclick="toggleDescription('carte')" style="cursor: pointer;"> Carte </h4>
-        <i id="description-carte" style="display: none;"><?php echo $imgString; ?></i>
-    <?php
+<div class="section zones">
+    <div class="container">
+        <h2 class="title is-3">Zones</h2>
+        <div class="box mb-4">
+            <h4 class="title is-5" onclick="toggleDescription('carte')" style="cursor: pointer;">Carte</h4>
+            <div id="description-carte" style="display: none;">
+                <?php echo $imgString; ?>
+            </div>
+        </div>
+        <?php
         // Display list of Zones
         foreach ($zones as $zone) {
-            $descritpion = $zone['description'];
+            $description = htmlspecialchars($zone['description']);
+            $zoneName = htmlspecialchars($zone['name']);
+            $zoneId = htmlspecialchars($zone['zone_id']);
+            $controllerBanner = (!empty($zone['controller_id']))
+                ? sprintf(
+                    '<span class="tag is-warning ml-2">Sous la bannière de %s %s</span>',
+                    htmlspecialchars($zone['firstname']),
+                    htmlspecialchars($zone['lastname'])
+                )
+                : '';
+            $knownSecrets = !empty($_SESSION['controller']['id'])
+                ? showcontrollerKnownSecrets($gameReady, $_SESSION['controller']['id'], $zone['zone_id'])
+                : '';
+            $ourControl = (!empty($_SESSION['controller']['id']) && $zone['holder_controller_id'] == $_SESSION['controller']['id'])
+                ? '<span class="tag is-danger ml-2">Sous notre contrôle</span><br>'
+                : '';
+
             echo sprintf('
-                <h3 onclick="toggleDescription(%2$s)" style="cursor: pointer;"> %1$s (%2$s) %4$s </h3>
-                <div id="description-%2$s" style="display: none;">
-                    %6$s
-                    <i>%3$s</i>
-                    %5$s
+                <div class="box mb-4">
+                    <h3 class="title is-5" onclick="toggleDescription(\'%2$s\')" style="cursor: pointer;">
+                        %1$s <span class="has-text-grey-light">(%2$s)</span> %3$s %6$s
+                    </h3>
+                    <div id="description-%2$s" style="display: none;">
+                        <p class="mb-2"><i>%4$s</i></p>
+                        %5$s
+                    </div>
                 </div>
                 ',
-                $zone['name'], $zone['zone_id'],
-                $descritpion,
-                (!empty($zone['controller_id']))
-                    ? sprintf('sous la bannière de %s %s', $zone['firstname'], $zone['lastname'])
-                    : '',
-                !empty($_SESSION['controller']['id']) ? showcontrollerKnownSecrets($gameReady, $_SESSION['controller']['id'], $zone['zone_id']) : '',
-                (!empty($_SESSION['controller']['id']) && $zone['holder_controller_id'] == $_SESSION['controller']['id'])? '<b>Sous notre controle <br></b>' : ''
+                $zoneName,
+                $zoneId,
+                $controllerBanner,
+                $description,
+                $knownSecrets,
+                $ourControl
             );
         }
-    ?>
+        ?>
+    </div>
 </div>
