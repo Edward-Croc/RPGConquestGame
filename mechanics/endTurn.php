21e2c4331e117fb4ad463e2d799a79fa7c9a881a
Edward PRITCHARD <epritchard@atreal.fr>
2025-08-31 11:38:39 +0200

    fix : error missing param in end turn

--- mechanics/endTurn.php
+++ mechanics/endTurn.php
@@ -40,7 +40,7 @@ require_once '../base/baseHTML.php';
             $reportAppendArray = [
                 'life_report' => sprintf(
                     "<strong>%s %s</strong> j'ai <strong>%s</strong> en investigation et <strong>%s/%s</strong> en attaque/défense.",
-                $timeDenominatorThis, $investigation, $attack, $defense
+                $timeDenominatorThis, $timeValue, $investigation, $attack, $defense
                 )
             ];
 
