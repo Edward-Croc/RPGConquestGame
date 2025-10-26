c7eba546a37a287f5cdc80af09402c6b861f9c3a
Edward PRITCHARD <epritchard@atreal.fr>
2025-10-26 12:23:54 +0100

    fix (workers) : reset to default action on move

--- workers/functions.php
+++ workers/functions.php
@@ -870,10 +870,11 @@ function moveWorker($pdo, $workerId, $zoneId) {
     }
     try{
         // UPDATE worker_actions values
-        $stmt = $pdo->prepare("UPDATE worker_actions SET zone_id = :zone_id, report = :report WHERE id = :id ");
+        $stmt = $pdo->prepare("UPDATE worker_actions SET zone_id = :zone_id, action_choice = 'passive', action_params = :action_params, report = :report WHERE id = :id ");
         $stmt->bindParam(':zone_id', $zoneId);
         $stmt->bindParam(':id', $action['id']);
         $stmt->bindParam(':report', $updatedReportJson);
+        $stmt->bindParam(':action_params', json_encode([]));
         $stmt->execute();
     } catch (PDOException $e) {
         echo __FUNCTION__."(): UPDATE workers Failed: " . $e->getMessage()."<br />";
